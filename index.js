6da325ba3259f2d62a5253bb346b207ac69b56ac
Sanjay <sunni12@uw.edu>
2019-05-04 14:36:39 -0700

    add js code

--- index.js
+++ index.js
@@ -1,18 +1,42 @@
+var categories = ['Animals',
+					'Arts, Culture, Humanities',
+					'Education',
+					'Environment',
+					'Health',
+					'Human Services',
+					'International',
+					'Human and Civil Rights',
+					'Religion',
+					'Community Development',
+					'Research and Public Policy']
+
+window.onload = function(){
+
+	generateIcons();
+
+};
+
+function generateIcons() {
+	$("interests").innerHTML = "";
+	for(let i = 0; i < list.books.length; i++) {
+		let newBook = document.createElement("div");
+		let newBookImg = document.createElement("img");
+		newBookImg.src= "books\" + list.books[i].folder + "\cover.jpg"
+		let newBookTitle = document.createElement("p");
+		newbookTitle.appendChild(document.createTextNode(list.books[i].title));
+		newBook.appendChild(newBookImg);
+		newBook.appendChild(newBookTitle);
+		newBook.addEventListener("click", function(){
+				//SOMETHING
+		});
+		$("allbooks").appendChild(newBook);
+	}
+}
+
 findCharity('WA', 'Seattle', 'Religion')
 //pls push
 
 function findCharity(state, city, category) {
-	var categories = ['Animals', 
-					  'Arts, Culture, Humanities', 
-					  'Education',
-					  'Environment',
-					  'Health',
-					  'Human Services',
-					  'International',
-					  'Human and Civil Rights',
-					  'Religion',
-					  'Community Development',
-					  'Research and Public Policy']
 	var index = categories.indexOf(category) + 1
 	var baseUrl = 'https://api.data.charitynavigator.org/v2/Organizations?'
 	state = '&state=' + state
@@ -31,4 +55,13 @@ function findCharity(state, city, category) {
 		console.log(obj);
 		//console.log(JSON.stringify(myJson));
 	});
+}
+
+/**
+* Returns the element that has the ID attribute with the specified value.
+* @param {string} id - element ID
+* @return {object} DOM object associated with id.
+*/
+function $(id) {
+		return document.getElementById(id);
 }
